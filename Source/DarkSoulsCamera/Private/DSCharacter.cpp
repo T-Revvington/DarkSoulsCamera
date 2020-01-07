d7f6d71b130d9e1e71b50131bcaf736a3c395b93
Trevor Roberts <TrevorRobertsin@gmail.com>
2020-01-07 17:32:37 -0500

    Dark Souls Final Camera

--- Source/DarkSoulsCamera/Private/DSCharacter.cpp
+++ Source/DarkSoulsCamera/Private/DSCharacter.cpp
@@ -43,37 +43,37 @@ ADSCharacter::ADSCharacter()
 
 	// Configure character movement (This is a comment)
     //Call GetCharacterMovement() and set the bOrientRotationToMovement to true on the UCharacterMovementComponent component that this ACharacter has
-	
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
     //Call GetCharacterMovement() and set the RotationRate to FRotator(0.0f, 540.0f, 0.0f)
-	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
     //Call GetCharacterMovement() and set the JumpZVelocity to 600.f
-	
+	GetCharacterMovement()->JumpZVelocity = 600.f;
     //Call GetCharacterMovement() and set the AirControl to 0.2f
-	
+	GetCharacterMovement()->AirControl = 0.2f;
 
 	// Create a camera boom (pulls in towards the player if there is a collision) (This is a comment)
     //CREATE the CameraLockArm Component
-	
+	CameraLockArm = CreateDefaultSubobject<UDSLockArmComponent>(TEXT("CameraLockArm"));
     //ATTACH the CameraLockArm to the RootComponent
-	
+	CameraLockArm->SetupAttachment(RootComponent);
     //SET the CameraLockArm's Relative Location to FVector(0.f, 0.f, 50.f)
-	
+	CameraLockArm->SetRelativeLocation(FVector(0.f, 0.f, 50.f));
 	
 	// Create a follow camera (This is a comment)
     //CREATE the FollowCamera Component
-	
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
     // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
     //ATTACH the FollowCamera to the CameraLockArm and this time, pass this USpringArmComponent::SocketName in as a second argument to the SetupAttachment() function, ALT+G into USpringArmComponent::SocketName and see what it is
-	
+	FollowCamera->SetupAttachment(CameraLockArm, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
     // Camera does not rotate relative to arm (This is a comment)
     //SET the FollowCamera's  bUsePawnControlRotation variable to false
-	
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	// Create target component (This is a comment)
     //CREATE the TargetComponent
-	
+	TargetComponent = CreateDefaultSubobject<UDSTargetComponent>(TEXT("TargetComponent"));
     //ATTACH the TargetComponent to the RootComponent
-	
+	TargetComponent->SetupAttachment(GetRootComponent());
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -105,19 +105,19 @@ void ADSCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputC
 void ADSCharacter::MoveForward(float Val)
 {
     //IF the Controller is NOT null AND Val is NOT null 
-	
+	if ((Controller != nullptr) && (Val != 0.0f))
 	{
 		// find out which way is forward (This is a comment)
         //DECLARE a FRotator called Rotation and using a Ternary operator, Check if CameraLockArm->CameraTarget IS nullptr, IF true CALL Controller->GetControlRotation() otherwise CALL (CameraLockArm->CameraTarget->GetOwner()->GetActorLocation() - GetActorLocation()).GetSafeNormal().Rotation()
-	
+		const FRotator Rotation = CameraLockArm->CameraTarget == nullptr ? Controller->GetControlRotation() : (CameraLockArm->CameraTarget->GetOwner()->GetActorLocation() - GetActorLocation()).GetSafeNormal().Rotation();
         //DECLARE a FRotator named YawRotation and set its YAW to Rotation.Yaw
-	
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
 		// get forward vector (This is a comment)
         //DECLARE a FVector called Direction and SET it to the return value of FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X), Getting its Direction on the Forward (Unreal X)
-	
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
         //CALL AddMovementInput passing in the DIrection and Val as the Scale Value
-	
+		AddMovementInput(Direction, Val);
 	}
     //ENDIF
 }
@@ -126,19 +126,19 @@ void ADSCharacter::MoveForward(float Val)
 void ADSCharacter::MoveRight(float Val)
 {
     //IF the Controller is NOT null AND Val is NOT null 
-	
+	if ((Controller != NULL) && (Val != 0.0f))
 	{
 		// find out which way is right (This is a comment)
          //DECLARE a FRotator called Rotation and using a Ternary operator, Check if CameraLockArm->CameraTarget IS nullptr, IF true CALL Controller->GetControlRotation() otherwise CALL (CameraLockArm->CameraTarget->GetOwner()->GetActorLocation() - GetActorLocation()).GetSafeNormal().Rotation()
-	
+		const FRotator Rotation = CameraLockArm->CameraTarget == nullptr ? Controller->GetControlRotation() : (CameraLockArm->CameraTarget->GetOwner()->GetActorLocation() - GetActorLocation()).GetSafeNormal().Rotation();
         //DECLARE a FRotator named YawRotation and set its YAW to Rotation.Yaw
-	
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
 		// get right vector (This is a comment)
          //DECLARE a FVector called Direction and SET it to the return value of FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X), Getting its Direction on the Right (Unreal Y)
-	
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
         //CALL AddMovementInput passing in the DIrection and Val as the Scale Value
-	
+		AddMovementInput(Direction, Val);
 	}
     //ENDIF
 }
@@ -147,37 +147,38 @@ void ADSCharacter::MoveRight(float Val)
 void ADSCharacter::Turn(float Val)
 {
     //DECLARE a float called TimeSinceLastTargetSwitch and assign it to the return value of GetWorld()->GetRealTimeSeconds() - LastTargetSwitchTime
-	
+	float TimeSinceLastTargetSwitch = GetWorld()->GetRealTimeSeconds() - LastTargetSwitchTime;
 
     //IF CameraLockArm->IsCameraLockedToTarget()
-	
+	if (CameraLockArm->IsCameraLockedToTarget())
 	{
 		// Should break soft-lock? (This is a comment)
         //IF CameraLockArm->bUseSoftLock is true AND FMath::Abs(Val) is GREATER THAN BreakLockMouseDelta
-	
+		if (CameraLockArm->bUseSoftLock && FMath::Abs(Val) > BreakLockMouseDelta)
 		{
             //CALL CameraLockArm->BreakTargetLock()
-	
+			CameraLockArm->BreakTargetLock();
             //SET BrokeLockTime to the return of GetWorld()->GetRealTimeSeconds()
-	
+			BrokeLockTime = GetWorld()->GetRealTimeSeconds();
             //SET CameraLockArm->bSoftlockRequiresReset to true
-	
+			CameraLockArm->bSoftlockRequiresReset = true;
 		}
         // Should try switch target? (This is a comment)
         //ELSE IF FMath::Abs(Val) Is GREATER THAN TargetSwitchMouseDelta AND TimeSinceLastTargetSwitch Is GREATER THAN TargetSwitchMinDelaySeconds		
-	
+		else if(FMath::Abs(Val) > TargetSwitchMouseDelta 
+			&& TimeSinceLastTargetSwitch > TargetSwitchMinDelaySeconds)	// Prevent switching multiple times using a single movement
 		{
             //IF Val is LESS than 0 (Ternary ??)
-	
+			if (Val < 0)
                 //CALL CameraLockArm->SwitchTarget() and Pass in EDirection::Left (switching it to the target on the left)
-	
+				CameraLockArm->SwitchTarget(EDirection::Left);
 			else //ELSE
                 //CALL CameraLockArm->SwitchTarget() and Pass in EDirection::Right (switching it to the target on the right)
-
+				CameraLockArm->SwitchTarget(EDirection::Right);
             //ENDIF
 
             //SET LastTargetSwitchTime to the return value of GetWorld()->GetRealTimeSeconds()
-
+			LastTargetSwitchTime = GetWorld()->GetRealTimeSeconds();
 		}
         //END ELSE IF
 	}
@@ -186,11 +187,11 @@ void ADSCharacter::Turn(float Val)
 	{
 		// If camera lock was recently broken by a large mouse delta, allow a cooldown time to prevent erratic camera movement (This is a comment)
         //DECLARE a bool called bRecentlyBrokeLock and SET it to (GetWorld()->GetRealTimeSeconds() - BrokeLockTime) < BrokeLockAimingCooldown
-
+		bool bRecentlyBrokeLock = (GetWorld()->GetRealTimeSeconds() - BrokeLockTime) < BrokeLockAimingCooldown;	
         //IF NOT bRecentlyBrokeLock
-
+		if(!bRecentlyBrokeLock)
             //CALL AddControllerYawInput() and pass in Val
-		//ENDIF
+			AddControllerYawInput(Val);	
 	}	
     //ENDIF
 }
@@ -199,10 +200,9 @@ void ADSCharacter::Turn(float Val)
 void ADSCharacter::LookUp(float Val)
 {
     //IF NOT CameraLockArm->IsCameraLockedToTarget()
-	
+	if (!CameraLockArm->IsCameraLockedToTarget())
         //CALL AddControllerPitchInput() and Pass in Val
 		AddControllerPitchInput(Val);
-	//ENDIF
 }
 
 //TODO 16: Implement Turning at given rate (Analog Stick Input)
@@ -210,32 +210,31 @@ void ADSCharacter::TurnAtRate(float Val)
 {	
 	// Ensure the analog stick returned to neutral since last target switch attempt (This is a comment)
     //IF FMath::Abs(Val) Is LESS THAN .1f
-	
+	if (FMath::Abs(Val) < .1f)
         //SET bAnalogSettledSinceLastTargetSwitch to true true
-	
-	//ENDIF
+		bAnalogSettledSinceLastTargetSwitch = true;
 
     //IF ( CameraLockArm->IsCameraLockedToTarget() is true AND ( FMath::Abs(Val) is GREATER THAN TargetSwitchAnalogValue) AND bAnalogSettledSinceLastTargetSwitch is true)
-
+	if (CameraLockArm->IsCameraLockedToTarget() && (FMath::Abs(Val) > TargetSwitchAnalogValue) && bAnalogSettledSinceLastTargetSwitch)
 	{
         //IF Val Is LESS THAN 0
-
+		if (Val < 0)
             //CALL CameraLockArm->SwitchTarget() and Pass in EDirection::Left (switching it to the target on the left)
-
+			CameraLockArm->SwitchTarget(EDirection::Left);
         //ELSE
 		else
             //CameraLockArm->SwitchTarget() and Pass in EDirection::Right (switching it to the target on the right)
-
+			CameraLockArm->SwitchTarget(EDirection::Right);
         //ENDIF
         //SET bAnalogSettledSinceLastTargetSwitch to false;
-
+		bAnalogSettledSinceLastTargetSwitch = false;
 	}
     //ELSE
 	else
 	{
 		// calculate delta for this frame from the rate information (This is a comment)
         //CALL AddControllerYawInput passing in Val * BaseTurnRate * GetWorld()->GetDeltaSeconds()
-
+		AddControllerYawInput(Val * BaseTurnRate * GetWorld()->GetDeltaSeconds());
 	}
     //ENDIF
 }
@@ -245,10 +244,9 @@ void ADSCharacter::LookUpAtRate(float Val)
 {
 	// calculate delta for this frame from the rate information (This is a comment)
     //IF NOT CameraLockArm->IsCameraLockedToTarget()
-
+	if (!CameraLockArm->IsCameraLockedToTarget())
         //CALL AddControllerPitchInput() Val * BaseLookUpRate * GetWorld()->GetDeltaSeconds()
-
-	//ENDIF
+		AddControllerPitchInput(Val * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
 //TODO 18: Update the Actor (We are using TickActor this time) 
@@ -258,24 +256,24 @@ void ADSCharacter::LookUpAtRate(float Val)
 void ADSCharacter::TickActor(float DeltaTime, enum ELevelTick TickType, FActorTickFunction& ThisTickFunction)
 {
     //CALL Super
-
+	Super::TickActor(DeltaTime, TickType, ThisTickFunction);
 
     //IF CameraLockArm->IsCameraLockedToTarget()
-
+	if (CameraLockArm->IsCameraLockedToTarget())
 	{
 		// Vector from player to target (This is a comment)
         //DECLARE a FVector called TargetVect and SET it to CameraLockArm->CameraTarget->GetComponentLocation() - CameraLockArm->GetComponentLocation()
-
+		FVector TargetVect = CameraLockArm->CameraTarget->GetComponentLocation() - CameraLockArm->GetComponentLocation();
         //DECLARE a FRotator called TargetRot and SET it to TargetVect.GetSafeNormal().Rotation()
-
+		FRotator TargetRot = TargetVect.GetSafeNormal().Rotation();
         //DECLARE a FRotator called FRotator and SET it to the return value of GetControlRotation()
-
+		FRotator CurrentRot = GetControlRotation();
         //DECLARE a FRotator and Interpolate (Lerp) it using the return value from Calling FMath::RInterpTo(CurrentRot, TargetRot, DeltaTime, LockonControlRotationRate);
-
+		FRotator NewRot = FMath::RInterpTo(CurrentRot, TargetRot, DeltaTime, LockonControlRotationRate);
 
 		// Update control rotation to face target (This is a comment)
         //CALL GetController()->SetControlRotation() passing in the New Rotation (NewRot)
-
+		GetController()->SetControlRotation(NewRot);
 	}
     //ENDIF
 }
--- Source/DarkSoulsCamera/Private/DSLockArmComponent.cpp
+++ Source/DarkSoulsCamera/Private/DSLockArmComponent.cpp
@@ -7,7 +7,7 @@
 #include "GameFramework/Pawn.h"
 
 //TODO 0: Macro definition of print() function, so we dont have to make the same call all over the place. MAKE CODING EASIER
-
+#define print(text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 4.f, FColor::Green, text)
 
 //TODO 1: Study the Components variable defaults
 UDSLockArmComponent::UDSLockArmComponent()
@@ -28,61 +28,63 @@ UDSLockArmComponent::UDSLockArmComponent()
 void UDSLockArmComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
     //CALL Super Function
-
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
     //IF IsCameraLockedToTarget()
-
+	if (IsCameraLockedToTarget())
 	{	
         //DRAW a Debug Sphere passing in the following arguments ( GetWorld(), CameraTarget->GetComponentLocation(), 20.f, 16, FColor::Red )
-
+		DrawDebugSphere(GetWorld(), CameraTarget->GetComponentLocation(), 20.f, 16, FColor::Red); //Draw target point
 
 		// Break lock if player is too far from target (This is a comment)
         //IF  (CameraTarget->GetComponentLocation() - GetComponentLocation()).Size() > MaxTargetLockDistance + CameraTarget->GetScaledSphereRadius()
-
+		if ((CameraTarget->GetComponentLocation() - GetComponentLocation()).Size() > MaxTargetLockDistance + CameraTarget->GetScaledSphereRadius())
 		{
             //IF bUseSoftLock is true
-
+			if (bUseSoftLock)
 			{
 				// Try switch to a new target in range (This is a comment)
                 //IF UDSTargetComponent* NewCameraTarget = GetLockTarget()
-
+				if (UDSTargetComponent* NewCameraTarget = GetLockTarget())
                     //CALL LockToTarget passing in the NewCameraTarget
-
-				//ELSE
+					LockToTarget(NewCameraTarget);
+				else //ELSE
                     //CALL BreakTargetLock()
-
+					BreakTargetLock();
                 //ENDIF
 			}
             //ELSE
+			else 
 			{ 
                 //CALL BreakTargetLock()
-
+				BreakTargetLock(); 
 			}
             //ENDIF
 		}
         //ENDIF
 	}
     //ELSE
+	else
 	{	
         // Attempt to auto target nearby enemy (This is a comment)
         //IF bUseSoftLock is true
-		
+		if (bUseSoftLock) 
 		{
             //IF UDSTargetComponent* NewCameraTarget = GetLockTarget()
-
+			if (UDSTargetComponent* NewCameraTarget = GetLockTarget())
 			{
                 // Soft-lock is reset? (This is a comment)
                 //IF NOT bSoftlockRequiresReset
-
+				if (!bSoftlockRequiresReset) 
                     //CALL LockToTarget passing in NewCameraTarget
-
+					LockToTarget(NewCameraTarget);
                 //ENDIF
 			}
             //ELSE // If player forcibly broke soft-lock, reset it when no target is within range (This is a comment)
-
+			else 
 			{
                 //SET bSoftlockRequiresReset to false
-
+				bSoftlockRequiresReset = false;
 			}
             //ENDIF
 		}
@@ -92,27 +94,27 @@ void UDSLockArmComponent::TickComponent(float DeltaTime, enum ELevelTick TickTyp
 
 	//Draw debug (This is how we will be visually debugging this semester. More to come ...) (This is a comment)
     //IF bDrawDebug is true
-
+	if (bDrawDebug)
 	{
         //FOREACH UDSTargetComponent* Target in TargetComponments (CALL GetTargetComponents(), there is no TargetComponments variable, Look at the GetTargetComponents() function)
-
+		for (UDSTargetComponent* Target : GetTargetComponents())
 		{
             //Draw Debug Line passing in  GetWorld(), GetOwner()->GetActorLocation(), Target->GetComponentLocation(), FColor::Green
-
+			DrawDebugLine(GetWorld(), GetOwner()->GetActorLocation(), Target->GetComponentLocation(), FColor::Green);
 		}
         //ENDFOR
 
 		//Draw target inclusion sphere (This is a comment)
         //Draw Debug Sphere passing in GetWorld(), GetOwner()->GetActorLocation(), MaxTargetLockDistance, 32, FColor::Cyan
-
+		DrawDebugSphere(GetWorld(), GetOwner()->GetActorLocation(), MaxTargetLockDistance, 32, FColor::Cyan);
 
         //Draw Debug String using the UKismetSystemLibrary
-
+		UKismetSystemLibrary::DrawDebugString(this, FVector::ZeroVector, bUseSoftLock ? "Soft-lock Enabled" : "Soft-lock Disabled", GetOwner(), FLinearColor::Green);
 		
         //IF bSoftlockRequiresReset is true
-
+		if (bSoftlockRequiresReset)
             //Draw Debug String using the UKismetSystemLibrary 
-
+			UKismetSystemLibrary::DrawDebugString(this, FVector(0.f, 0.f, -10.f), "Soft-lock Requires Reset", GetOwner(), FLinearColor::Green);
         //ENDIF
 	}
     //ENDIF
@@ -123,10 +125,10 @@ void UDSLockArmComponent::ToggleCameraLock()
 {
     // Soft-lock supersedes player input (This is a comment)
     //IF bUseSoftLock is true
-
+	if (bUseSoftLock)  
 	{
         //SET bSoftlockRequiresReset to false
-
+		bSoftlockRequiresReset = false;
         //RETURN
 		return;
 	}
@@ -134,25 +136,25 @@ void UDSLockArmComponent::ToggleCameraLock()
 
 	//If CameraTarget is set, unset it (This is a comment)
     //IF IsCameraLockedToTarget()
-
+	if (IsCameraLockedToTarget())
 	{
         //CALL BreakTargetLock()
-
+		BreakTargetLock();
         //RETURN
-
+		return;
 	}
     //ENDIF
 
     //DECLARE a UDSTargetComponent* called NewCameraTarget and SET it to the return value of GetLockTarget()
-
+	UDSTargetComponent* NewCameraTarget = GetLockTarget();
 
     //IF NewCameraTarget NOT EQUAL nullptr
-
+	if(NewCameraTarget != nullptr)
 	{
         //PRINT "Testing"
-
+		print(TEXT("Testing"));
         //CALL LockToTarget() passing in NewCameraTarget
-
+		LockToTarget(NewCameraTarget);
 	}
     //ENDIF
 }
@@ -161,23 +163,23 @@ void UDSLockArmComponent::ToggleCameraLock()
 void UDSLockArmComponent::ToggleSoftLock()
 {
     //TOGGLE bUseSoftLock
-
+	bUseSoftLock = !bUseSoftLock;
 
     //IF bUseSoftLock is true
-
+	if (bUseSoftLock)
 	{
         //PRINT "Soft-lock enabled"
-
+		print(TEXT("Soft-lock enabled"));
         //SET bSoftlockRequiresReset to false;
-
+		bSoftlockRequiresReset = false;
 	}
     //ELSE
-
+	else
 	{
         //CALL BreakTargetLock()
-
+		BreakTargetLock();
         //PRINT "Soft-lock disabled"
-
+		print(TEXT("Soft-lock disabled"));
 	}
     //ENDIF
 }
@@ -185,9 +187,9 @@ void UDSLockArmComponent::ToggleSoftLock()
 void UDSLockArmComponent::LockToTarget(UDSTargetComponent* NewTargetComponent)
 {
     //SET the CameraTarget to the NewTargetComponent
-
+	CameraTarget = NewTargetComponent;
     //SET bEnableCameraRotationLag to true
-
+	bEnableCameraRotationLag = true;
 	//GetCharacterMovement()->bOrientRotationToMovement = false;
 }
 
@@ -195,13 +197,13 @@ void UDSLockArmComponent::LockToTarget(UDSTargetComponent* NewTargetComponent)
 void UDSLockArmComponent::BreakTargetLock()
 {
     //IF IsCameraLockedToTarget()
-
+	if (IsCameraLockedToTarget())
 	{
         //SET CameraTarget to nullptr
-
+		CameraTarget = nullptr;
 		//GetController()->SetControlRotation(FollowCamera->GetForwardVector().Rotation()); //REMOVE
         //SET bEnableCameraRotationLag to false
-
+		bEnableCameraRotationLag = false;
 		//GetCharacterMovement()->bOrientRotationToMovement = true; //REMOVE
 	}
     //ENDIF
@@ -211,83 +213,84 @@ void UDSLockArmComponent::BreakTargetLock()
 UDSTargetComponent* UDSLockArmComponent::GetLockTarget()
 {
     //DECLARE a TArray of Type UDSTargetComponent* called AvailableTargets and Assign it the return value of GetTargetComponents()
-
+	TArray<UDSTargetComponent*> AvailableTargets = GetTargetComponents();
     //IF the AvailableTargets is empty
-
+	if (AvailableTargets.Num() == 0)
         //RETURN nullptr
-
+		return nullptr;
     //ENDIF
 
 	// Get the target with the smallest angle difference from the camera forward vector (This is a comment)
     //DECLARE a float called ClosestDotToCenter and assign it to 0
-
+	float ClosestDotToCenter = 0.f;
     
     //DECLARE a UDSTargetComponent called TargetComponent and initialize it with nullptr as a default value
-
+	UDSTargetComponent* TargetComponent = nullptr;
 
     /*
         The dot product can be used to find the length of a vector or the angle between two vectors.
         The cross product is used to find a vector which is perpendicular to the plane spanned by two vectors.
     */
     //FOR i = 0, i < AvailableTargets.Num, i++
-
+	for (int32 i = 0; i < AvailableTargets.Num(); i++)
 	{
         //DECLARE a float called Dot, and assign it to the return value of the FVector::DotProduct(GetForwardVector(), (AvailableTargets[i]->GetComponentLocation() - GetComponentLocation()).GetSafeNormal())
-
+		float Dot = FVector::DotProduct(GetForwardVector(), (AvailableTargets[i]->GetComponentLocation() - GetComponentLocation()).GetSafeNormal());
         //IF the Dot product is GREATER than the ClosestDotToCenter
-
+		if (Dot > ClosestDotToCenter)
 		{
             //SET the ClosestDotToCenter to Dot
-
+			ClosestDotToCenter = Dot;
             //SET the TargetComponent to AvailableTargets[i]
-
+			TargetComponent = AvailableTargets[i];
 		}
         //ENDIF
 	}
     //ENDFOR
 
     //RETURN TargetComponent
-
+	return TargetComponent;
 }
 
 //TODO 4: Switch to Locked targets (TArray documentation)
 void UDSLockArmComponent::SwitchTarget(EDirection SwitchDirection)
 {
     //IF the camera is not locked to a target then return
-
-	//ENDIF
+	if (!IsCameraLockedToTarget()) 
+        return;
 
     //DECLARE a TArray of type UDSTargetComponent* called AvailableTargets and assign it to the return value of GetTargetComponents()
-	
+	TArray<UDSTargetComponent*> AvailableTargets = GetTargetComponents();	// Get targets within lock-on range	
 
     // Must have an existing camera target and 1 additional target (This is a comment)
     //IF The number of AvailableTargets is LESS than 2, then return, Must have an existing camera target and 1 additional target
-	
-	//ENDIF
+	if (AvailableTargets.Num() < 2) 
+        return;	
 
     //Calculate the Current Target Direction (This is a comment)
     //DECLARE a FVector called CurrentTargetDir and calculate its direction --> (CameraTarget->GetComponentLocation() - GetComponentLocation()).GetSafeNormal() //These will not be provided in ICA's
-
+	FVector CurrentTargetDir = (CameraTarget->GetComponentLocation() - GetComponentLocation()).GetSafeNormal();
 
     //DECLARE a TArray of type UDSTargetComponent* called ViableTargets
-
+	TArray<UDSTargetComponent*> ViableTargets;
 
     //FOREACH Target in AvailableTargets
-
+	for (UDSTargetComponent* Target : AvailableTargets)
 	{
 		//  Don't consider current target as a switch target (This is a comment)
         //IF the Target is EQUAL to CameraTarget
-
+		if (Target == CameraTarget) 
+            continue;
            //continue
         //ENDIF
 
         //Calculate the Target Direction (This is a comment)
         //DECLARE a FVector called TargetDir and Assign it to (Target->GetComponentLocation() - GetComponentLocation()).GetSafeNormal(); //These will not be provided in ICA's
-
+		FVector TargetDir = (Target->GetComponentLocation() - GetComponentLocation()).GetSafeNormal();
         
         //Obtain the Cross Product between Current Target Direction and the Target Direction (This is a comment)
         //DECLARE a FVector called Cross and Assign it to FVector::CrossProduct(CurrentTargetDir, TargetDir)
-
+		FVector Cross = FVector::CrossProduct(CurrentTargetDir, TargetDir);
 
         //SwitchDirection is EQUAL to EDirection::Right AND Crozz.Z is GREATER than 0 (This is a comment)
         //IF the SwitchDirection is EQUAL to EDirection::Left AND Crozz.Z is LESS than 0 OR 
@@ -295,64 +298,68 @@ void UDSLockArmComponent::SwitchTarget(EDirection SwitchDirection)
 			|| (SwitchDirection == EDirection::Right && Cross.Z > 0.f))	// Positive Z indicates right
 		{
             //ADD the Target to the ViableTargets as a Unique Element i.e look up TArray documentation
-
+			ViableTargets.AddUnique(Target);
 		}
         //ENDIF
 	}
     //END FOREACH
 
     //IF There are NO ViableTargets, return 
-
+	if (ViableTargets.Num() == 0) 
+        return;
     //ENDIF
 	/*
 	Select the target with the smallest angle difference to the current target (This is a comment)
 	*/
     //DECLARE a int32 called BBestDotIdx and assign it to 0
-
+	int32 BestDotIdx = 0;
     //FOR i = 1, i < Number of ViableTargets, i++
-
+	for (int32 i = 1; i < ViableTargets.Num(); i++)
 	{
         //DECLARE a float called BestDot and assign it to the Dot Product between the CurrentTargetDir and the Direction of the ViableTarget at the BestDotIdx --> FVector::DotProduct(CurrentTargetDir, (ViableTargets[BestDotIdx]->GetComponentLocation() - GetComponentLocation()).GetSafeNormal());
-
+		float BestDot = FVector::DotProduct(CurrentTargetDir, 
+                                            (ViableTargets[BestDotIdx]->GetComponentLocation() - GetComponentLocation()).GetSafeNormal());
         //DECLARE a float called TestDot and assign it to the DOt Product between the CurrentTargetDir and the Direction of the ViableTarget at i --> 		FVector::DotProduct(CurrentTargetDir, (ViableTargets[i]->GetComponentLocation() - GetComponentLocation()).GetSafeNormal());
-
+        float TestDot = FVector::DotProduct(CurrentTargetDir, 
+                                            (ViableTargets[i]->GetComponentLocation() - GetComponentLocation()).GetSafeNormal());
 
 		// Higher dot product indicates this target vector has a smaller angle than the previous best (This is a comment)
         //IF TestDot is GREATER than BestDot
-
+		if (TestDot > BestDot)
             //SET BestDotIdx to i
-
+			BestDotIdx = i;
         //ENDIF
 	}
     //ENDFOR
 
     //CALL LockToTarget( ViableTargets[BestDotIdx] )
-
+	LockToTarget(ViableTargets[BestDotIdx]);
 }
 
 //TODO 2: Search and return targetable components
 TArray<UDSTargetComponent*> UDSLockArmComponent::GetTargetComponents()
 {
     //DECLARE a TArray of Type UPrimitiveComponent* called TargetPrims
-
+	TArray<UPrimitiveComponent*> TargetPrims;
     //DEClARE a TArray of type TEnumAsByte<EObjectTypeQuery> called ObjectTypes and add one element {EObjectTypeQuery::ObjectTypeQuery2}
-
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes = { EObjectTypeQuery::ObjectTypeQuery2 }; // World dynamic object type
 
 	// Overlap check for targetable component
     //CALL UKismetSystemLibrary::SphereOverlapComponents(Owner, ComponentLocation, MaxTargetLockDistance, TArrayOfObjectTypes, UDSTargetComponent's Class, TArrayOfActorsToIgnore, OutComponents)
-
+	UKismetSystemLibrary::SphereOverlapComponents(GetOwner(),GetComponentLocation(),MaxTargetLockDistance, ObjectTypes,UDSTargetComponent::StaticClass(),TArray<AActor*>{GetOwner()},TargetPrims);
 
     //DECLARE a TArray of type UDSTargetComponent* called TargetComps
-
+	TArray<UDSTargetComponent*> TargetComps;
     //FOREACH component in TagrgetPrims
+	for (UPrimitiveComponent* Comp : TargetPrims)
 	{
         //ADD the component to TargetComps
-
+		TargetComps.Add(Cast<UDSTargetComponent>(Comp));
 	}
     //END FOREACH
 
     //RETURN TargetComps
-
+	return TargetComps;
 }
 
 //TODO 7: Return true is Camera is locked to a target otherwise false. Don't use a if statement 
@@ -360,5 +367,5 @@ bool UDSLockArmComponent::IsCameraLockedToTarget()
 {
     //RETURN CameraTarget NOT EQUAL TO nullptr
     //return false; //this is here temporarily so it compiles, should be replace by actual working code
-
+	return CameraTarget != nullptr;
 }
